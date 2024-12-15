9a83d03f3e377303e14c6e9ca3af6900d2a01efe
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-12-15 14:58:51 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,6 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDfwO7zz5pdCa0zlThbGbhtLIO3eey76No",
-        authDomain: "blogging-website-bf766.firebaseapp.com",
-        projectId: "blogging-website-bf766",
-        storageBucket: "blogging-website-bf766.firebasestorage.app",
-        messagingSenderId: "364944894513",
-        appId: "1:364944894513:web:4b519aa21f9ca7aedcc310"
+     //firebase api key
 };
 
 
