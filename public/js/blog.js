dd7ef4e5a3f54be83fa6550424f54103139c49dc
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-12-15 14:58:01 +0530

    Update blog.js

--- public/js/blog.js
+++ public/js/blog.js
@@ -5,12 +5,7 @@ const { getStorage, ref, getDownloadURL } = require('firebase/storage');
 const { initializeApp } = require('firebase/app');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDfwO7zz5pdCa0zlThbGbhtLIO3eey76No",
-    authDomain: "blogging-website-bf766.firebaseapp.com",
-    projectId: "blogging-website-bf766",
-    storageBucket: "blogging-website-bf766.appspot.com",
-    messagingSenderId: "364944894513",
-    appId: "1:364944894513:web:4b519aa21f9ca7aedcc310"
+     //firebase api key
 };
 
 
